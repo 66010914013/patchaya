bc2bc2a4f6f6e062eb62f620c04610e1c78ce430
66010914013 <66010914013@msu.ac.th>
2026-02-18 14:47:43 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>พัชยา ศรีมุกดา (โฟม)</title>
+</head>
+<body>
+    <h1>พัชยา ศรีมุกดา (โฟม)
+</h1>
+<img src= "images/1.jpg"width="305" >
+    
+</body>
+</html>
