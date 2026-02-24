3666433e1c2f4ae46cb809e4ab6996f6afd69aaf
66010914013 <66010914013@msu.ac.th>
2026-02-24 23:03:25 +0700

    a

---
+++ connectdb.php
@@ -0,0 +1,8 @@
+<?php
+		$host = "localhost";
+		$user = "root";
+		$pwd = "zx123456";
+		$db = "4013";
+		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
+		mysqli_query($conn, "SET NAMES utf8");
+?>
