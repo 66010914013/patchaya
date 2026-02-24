19500c5036476f072212b982284d99e81350913d
66010914013 <66010914013@msu.ac.th>
2026-02-25 00:03:17 +0700

    g

--- g/connectdb.php
+++ g/connectdb.php
@@ -2,7 +2,7 @@
 		$host = "localhost";
 		$user = "root";
 		$pwd = "zx123456";
-		$db = "4013";
+		$db = "4013db";
 		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 		mysqli_query($conn, "SET NAMES utf8");
 ?>
